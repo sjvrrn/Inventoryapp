766dd901c8485a10b07e93cf4778648da07584d2
NarayanaErp <narayana@mavenerp.in>
2026-05-09 18:41:20 +0530

    react vite installation
    react vite installation

---
+++ Inventoryapp/resources/js/bootstrap.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+window.axios = axios;
+
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+window.axios.defaults.withCredentials = true;
+
+// Optional base URL
+window.axios.defaults.baseURL = 'http://127.0.0.1:8000';
