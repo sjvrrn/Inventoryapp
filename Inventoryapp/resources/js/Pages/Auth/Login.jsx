e5580920621fa44df8155af0e0cb8c3ff3abd800
NarayanaErp <narayana@mavenerp.in>
2026-05-10 07:56:33 +0530

    register user link in login page
    register user link in login page

--- Inventoryapp/resources/js/Pages/Auth/Login.jsx
+++ Inventoryapp/resources/js/Pages/Auth/Login.jsx
@@ -90,6 +90,12 @@ export default function Login({ status, canResetPassword }) {
                         </Link>
                     )}
 
+                    <Link
+                        href={route('register')}
+                        className="rounded-md text-sm text-gray-600 underline hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                    >
+                        Not registered?
+                    </Link>
                     <PrimaryButton className="ms-4" disabled={processing}>
                         Log in
                     </PrimaryButton>
